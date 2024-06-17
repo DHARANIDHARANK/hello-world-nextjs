911869919a91e7651fd1a363fddc7c36883b1d0a
DHARANIDHARAN K <sankarprasath111@gmail.com>
2024-06-17 16:16:57 +0530

    adding files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  // Custom webpack configuration
+  webpack: (config, { isServer }) => {
+    // Example of custom Webpack configuration
+    if (!isServer) {
+      config.resolve.fallback = { ...config.resolve.fallback, fs: false };
+    }
+    return config;
+  },
+  // Additional Next.js specific configurations can be added here
+  // such as images, redirects, rewrites, etc.
 };
 
 export default nextConfig;
